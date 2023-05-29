047354699177bb9588a67d11a195a2ca1ebf9611
Hamza fAHAM <hamzafaham111@gmail.com>
2023-05-29 17:19:24 +0500

    made some changes

--- pages/_app.js
+++ pages/_app.js
@@ -5,8 +5,12 @@ import Head from 'next/head'
 export default function App({ Component, pageProps }) {
   return (<>
     <Head>
-      <title>Hamza Faham</title>
-    </Head>
+      <title>Hamza Faham - Software Engineer</title>
+        <meta name="description" content="I am a Software Engineer having two years of Full stack development experience. Explore my portfolio to see my projects and skills." /> 
+        <meta property="og:title" content="Hamza Faham - Software Engineer" />
+        <meta property="og:description" content="I am a Software Engineer having two years of Full stack development experience. Explore my portfolio to see my projects and skills." />
+      </Head>
+    
     <NavBar />
     <Component {...pageProps} />
     <Footer />
