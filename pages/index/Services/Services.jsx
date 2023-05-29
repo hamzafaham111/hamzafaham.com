9bf666d6b179cf58abfa532994f472ebb11c5bb2
Hamza fAHAM <hamzafaham111@gmail.com>
2023-05-29 16:44:14 +0500

    made some changes

--- pages/index/Services/Services.jsx
+++ pages/index/Services/Services.jsx
@@ -3,6 +3,7 @@ import Image from "next/image";
 // import WebIcon from "./Assets/services icons/website.png";
 // import DesignIcon from "./Assets/services icons/design.png";
 // import DocumentIcon from "./Assets/services icons/documentation.png";
+import Style from '../../../styles/Services.module.css'
 const Services = () => {
   return (
     <>
@@ -15,6 +16,7 @@ const Services = () => {
             <div className="w-2 mt-2 h-0 border border-b-4 border-pink-600"></div>
           </div>
         </div>
+
         <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 md:gap-6 mt-8">
           <div className="bg-gray-900 hover:bg-pink-900 cursor-pointer h-44 flex flex-col p-4 hover:pt-2 transition duration-500 ease-in-out">
             <Image
@@ -25,7 +27,7 @@ const Services = () => {
             <span className="text-gray-300 font-bold my-2">
               Web Development
             </span>
-            <span className="text-gray-300 text-xs sm:text-sm leading-4 overflow-auto">
+            <span className={`text-gray-300 text-xs sm:text-sm leading-4 overflow-auto ${Style.custom_scroll}`}>
               I am a full-stack web developer having 2 years of experience in
               MERN stack.
             </span>
@@ -37,7 +39,7 @@ const Services = () => {
               width={30}
             />
             <span className="text-gray-300 font-bold my-2">UI/UX Design</span>
-            <span className="text-gray-300 text-xs sm:text-sm leading-4 overflow-auto">
+            <span className={`text-gray-300 text-xs sm:text-sm leading-4 overflow-auto ${Style.custom_scroll}`}>
               UI/UX design refers to the process of designing the user interface
               of a product.
             </span>
@@ -49,7 +51,7 @@ const Services = () => {
               width={30}
             />
             <span className="text-gray-300 font-bold my-2">System Design</span>
-            <span className="text-gray-300 text-xs sm:text-sm leading-4 overflow-auto">
+            <span className={`text-gray-300 text-xs sm:text-sm leading-4 overflow-auto ${Style.custom_scroll}`}>
               I have been providing system design services (HLD & LLD) for the
               past 2 years as a freeancer.
             </span>
@@ -61,7 +63,7 @@ const Services = () => {
               width={30}
             />
             <span className="text-gray-300 font-bold my-2">Documentation</span>
-            <span className="text-gray-300 text-xs sm:text-sm leading-4 overflow-auto">
+            <span className={`text-gray-300 text-xs sm:text-sm leading-4 overflow-auto ${Style.custom_scroll}`}>
               I can make your project documentation and project report, I have 2
               years of experience in it. with less then 10% of plagerisem
             </span>
@@ -73,7 +75,7 @@ const Services = () => {
               width={30}
             />
             <span className="text-gray-300 font-bold my-2">Deployment</span>
-            <span className="text-gray-300 text-xs sm:text-sm leading-4 overflow-auto">
+            <span className={`text-gray-300 text-xs sm:text-sm leading-4 overflow-auto ${Style.custom_scroll}`}>
               I am a full-stack web developer having 2 years of experience in
               MERN stack.
             </span>
