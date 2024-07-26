776033302012d52ba9d4a5d20215d7f92f3d7b17
hamza-ziwotech <150322821+hamza-ziwotech@users.noreply.github.com>
2024-07-26 15:17:53 +0500

    made some changes

--- pages/blogs/index.js
+++ pages/blogs/index.js
@@ -14,28 +14,12 @@ const index = () => {
         <h1 className="text-3xl text-gray-100 font-semibold leading-none mb-4">
           Latest uploaded
         </h1>
-        {/* <div className="flex">
-          <div className="w-12 mr-1 mt-2 h-0 border border-b-4 border-pink-200"></div>
-          <div className="w-2 mt-2 h-0 border border-b-4 border-pink-200"></div>
-        </div>
-        <div className="flex gap-2 mt-4">
-          <div className="w-3/6 h-[300px] bg-gray-900">
-            
-          </div>
-          <div className="w-3/6 h-[300px] bg-gray-900">
-            
-          </div>
-        </div> */}
         <Caracol/>
       </div>
       <div className="">
         <h1 className="text-3xl text-gray-100 font-semibold mt-8 leading-none">
           Most Popular
         </h1>
-        {/* <div className="flex">
-          <div className="w-12 mr-1 mt-2 h-0 border border-b-4 border-pink-200"></div>
-          <div className="w-2 mt-2 h-0 border border-b-4 border-pink-200"></div>
-        </div> */}
         <div className="flex gap-2 mt-4">
           <div className="w-3/6 h-[300px] bg-gray-900 duration-700 ease-in-out">
             
